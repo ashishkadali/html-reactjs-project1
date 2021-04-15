02dac026f6bebe3eb220a8db32d99c262e32392a
ashishkadali <ashishkadali7@msitprogram.net>
2021-04-15 22:56:39 +0530

    quiz

---
+++ Qiz/main.js
@@ -0,0 +1,13 @@
+const names=document.getElementById("name");
+const numbers=document.getElementById("number");
+const buttons=document.getElementById("button");
+
+
+function save(){
+    const key=names.value;
+    const value=numbers.value;
+
+
+    console.log(key, value);
+
+}
